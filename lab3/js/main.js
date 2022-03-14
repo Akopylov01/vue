f425b37f1a48b3f6707b92846aa69c87c51ec74f
Akopylov01 <alexanderkopylov01@gmail.com>
2022-03-14 20:15:09 +0700

    Недоработанные функции

--- lab3/js/main.js
+++ lab3/js/main.js
@@ -4,46 +4,108 @@ let app = new Vue({
         name:'',
         isStart: false,
         scores: 0,
-        fish:0,
-        fishImg:'',
-        time: 5,
+        result: [],
         fishes: [],
+        class: ['fish1','fish2','fish3'],
+        time: 60,
+        interval:'',
     },
     methods: {
         startGame: function(){
-            this.isStart=!this.isStart;
-            if(this.isStart){
-                this.timer();
-            }
+            this.isStart = true;
+            this.resetTimer();
+            this.timer();
+            
+
         },
         timer: function(){
-            let interval = setInterval(() => {
-                if (this.time==0){
-                    this.isStart=false;
-                    this.time = 5;
-                }
+            this.interval = setInterval(() => {
+                this.time -- ;
+
+                if (this.time <= 0){
+                    this.endGame(this.interval);
+                } 
                 else{
-                    --this.time;
+                    this.fishCreate();
                 }
+
             }, 1000);
 
         },
+        endGame: function(){
+            this.isStart = false;
+            this.resetTimer();
+            this.result.push({
+                name: this.name,
+                score: this.score
+            });
+            this.name='';
+
+        },
+        resetTimer: function(){
+            return clearInterval(this.interval);
+        },
         fishCreate: function(){
-            setInterval(() => {
-                this.fish = Math.floor(Math.random() * 3);
-                if(this.fish == 1){
-                    this.fishImg = '../media/рыба1.jpg';
+                let type = Math.floor(Math.random() * 3);
+                let fish = {
+                    type:type, 
+                    top: Math.floor(Math.random() * 640),
+                    left: Math.floor(Math.random() * (1000 - 150)),
+                    turn:false 
                 }
-                else if(this.fish == 2){
-                    this.fishImg = '../media/рыба2.jpg';
+                if(fish.type == 1){
+                    fish.img = 'media/fish1.png';
+                    fish.class ='fish1';
+                    fish.width = 50;
+	                fish.height = 100;
+
+                }
+                else if(fish.type == 2){
+                    fish.img = 'media/fish2.png';
+                    fish.class ='fish2';
+                    fish.width = 60;
+	                fish.height = 120;
+
                 }
                 else{
-                    this.fishImg = '../media/рыба3.jpg';
+                    fish.img = 'media/fish3.png';
+                    fish.class ='fish3';
+                    fish.width = 75;
+	                fish.height = 150;
+
                 }
-            })
+                this.fishes.push(fish);
+                this.makeTurn(fish);
+            }
         },
+        makeTurn: function(obj){
+            setInterval(() => {
+                if(obj.left == 0){
+                    obj.turn = false;
+                }
+                else{
+                    obj.turn = true;
+                }
+                if(obj.turn){
+                    obj.left--;
+                }
+                else{
+                    obj.left++;
+                }
+            }, 100)
+        },
+        takeFish: function(index){
+            if(this.fishes[index].type==1){
+                this.scores += 35;
+            }
+            else if(this.fishes[index].type==2){
+                this.scores += 55;
 
+            }
+            else if(this.fishes[index].type==3){
+                this.scores += 70;
 
-
-    },
+            }
+            this.fishes.splice(index, 1)
+        },
 });
