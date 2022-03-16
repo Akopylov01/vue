b29b69fd20e841ec02ded7edd00370ccef2434c1
Akopylov01 <alexanderkopylov01@gmail.com>
2022-03-16 21:45:17 +0700

    1/2lab

--- lab4/js/main.js
+++ lab4/js/main.js
@@ -3,42 +3,87 @@
 let app = new Vue({
     el: '#app',
     data: {
-        tasks:[
-            {
-                name:'',
-                task:[],
-            },
-
-        ],
+        tasks:[],
         searchStr:'',
         showModal:false,
         showTask: false,
         taskListName: '',
         taskName:'',
+        changeListName:'',
+        editTaskListModal: false,
+        tempId:'',
+        classColor:'elemHead',
+        color:'white',
+        fontColor:'',
     },
-    computed: {
-        searchTask() {
-          if (this.searchStr) {
-            return this.tasks.filter(item => {
-                return item.name.includes(this.searchStr);
-            });
-          }
-          return this.tasks;
-        }
-    },
+    // computed: {
+    //     searchTask() {
+    //       if (this.searchStr) {
+    //         return this.tasks.filter(item => {
+    //             return item.name.includes(this.searchStr);
+    //         });
+    //       }
+    //       return this.tasks;
+    //     }
+    // },
     methods: {
-        addTaskList: function(){
-            this.tasks = [];
-            this.tasks.push({ name:this.taskListName, task:'' });
-            this.showTask = this.tasks.length;
-            this.taskListName = '';
-            this.showModal = false;
-            console.log(this.tasks);
+        editColor: function(id){
+            if(this.tasks[id].color = 'white'){
+                this.tasks[id].color = 'black';
+                this.tasks[id].fontColor = 'white';
+
+            }
+            else{
+                this.tasks[id].color = 'white';
+                this.tasks[id].fontColor = 'black';
+
+            }
         },
-        addTask: function(){
-            this.tasks.push({ task:this.taskName });
+        classNameHead: function(id) {
+            if(this.tasks[id].swapColor == true) {
+                this.colorClass='elemHeadBlack';
+            } 
+            else 
+            {
+                this.colorClass='elemHead';
+            }
+        },
+        addTaskList: function(){
+            let date = new Date().toLocaleString();
+            console.log(this.tasks);
+            if (this.taskListName.trim() == '') 
+            { 
+                return false;
+            }
+            this.tasks.push({ name:this.taskListName, task:[], date: date, color:'white'});
+            this.showTask = this.tasks.length;
+            this.showModal = false;
+            this.taskListName = '';
+        },
+        addTask: function(id){
+            this.tasks[id].task.push(this.taskName);
+            console.log(this.tasks);
+            this.taskName = '';
+
+        },
+        editTaskListModalFunc: function(id){
+            this.tempId = id;
+            this.changeListName = this.tasks[id].name;
+            this.editTaskListModal = true;
+        },
+        editTaskList: function(id){
+            if (this.changeListName.trim() == '') 
+            { 
+                return false; 
+            }
+            this.tasks[this.tempId].name = this.changeListName;
+            this.editTaskListModal = false;
+        },
+        deleteTaskList: function(id){
+            this.tasks.splice(id, 1);
         }
-    }
-    
+        
+    },
+
         
 });
